3624afb2b28251112cfe987b8976161c5d5e547d
Mat Sadler <mat@redstudio.co.uk>
2012-07-03 13:57:24 +0000

    first commit, using drag dealer

---
+++ index.php
@@ -0,0 +1,126 @@
+
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+
+<!-- Use the .htaccess and remove these lines to avoid edge case issues.
+       More info: h5bp.com/i/378 -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<title></title>
+<meta name="description" content="">
+
+<!-- Mobile viewport optimized: h5bp.com/viewport -->
+<meta name="viewport" content="width=device-width">
+
+<!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
+<link rel="stylesheet" href="css/dragdealer.css">
+
+<!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
+
+<!-- All JavaScript at the bottom, except this Modernizr build.
+       Modernizr enables HTML5 elements & feature detects for optimal performance.
+       Create your own custom Modernizr build: www.modernizr.com/download/ -->
+<script src="js/libs/modernizr-2.5.3.min.js"></script>
+</head>
+<body>
+	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+
+	<div class="top-spaced">
+		<div id="slideshow" class="dragdealer">
+			<div class="handle" style="left: 20px;">
+				<div class="slide img1"></div>
+				<div class="slide img2"></div>
+				<div class="slide img3"></div>
+				<div class="slide img4"></div>
+				<div class="slide img5"></div>
+			</div>
+		</div>
+	</div>
+
+	<div id="slideshow-menu-wrapper" class="top-spaced right-float">
+		<div id="slideshow-menu-cursor" class="cursor" style="top: 66px;"></div>
+		<ul id="slideshow-menu">
+			<li><a id="slideshow-photo-1" href="#photo1"> <span class="title">Lorem</span>
+					<span class="description">Lorem ipsum dolor sit.</span>
+			</a>
+			</li>
+			<li><a id="slideshow-photo-2" href="#photo2"> <span class="title">Ipsum</span>
+					<span class="description">Lorem ipsum dolor sit.</span>
+			</a>
+			</li>
+			<li><a id="slideshow-photo-3" href="#photo3"> <span class="title">Dolor</span>
+					<span class="description">Lorem ipsum dolor sit.</span>
+			</a>
+			</li>
+			<li><a id="slideshow-photo-4" href="#photo4"> <span class="title">Sit
+						amet</span> <span class="description">Lorem ipsum dolor sit.</span>
+			</a>
+			</li>
+			<li><a id="slideshow-photo-5" href="#photo5"> <span class="title">Sit
+						amet</span> <span class="description">Lorem ipsum dolor sit.</span>
+			</a>
+			</li>
+			
+			
+			<li><a id="slideshow-next" href="#photo5"> <span class="title">Sit
+						amet</span> <span class="description">next.</span>
+			</a>
+			</li>
+			
+			
+			<li><a id="slideshow-previous" href="#photo5"> <span class="title">Sit
+						amet</span> <span class="description">previous.</span>
+			</a>
+			</li>
+		</ul>
+	</div>
+	
+	
+	<div id="handPhone" style="background:#999; width:200px; height:200px; position:absolute; bottom:-200px; right:0;">
+	
+	</div>
+	
+		<div id="finishedShopping" style="background:#999; width:200px; height:200px; position:absolute; top:300px; left:450px; opacity:0.1;">
+	
+	</div>
+
+	<!-- JavaScript at the bottom for fast page loading -->
+
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	<script
+		src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+
+	<!-- scripts concatenated and minified via build script -->
+	<script src="js/dragdealer.js"></script>
+	<script src="js/script.js"></script>
+	<script>
+  
+
+  </script>
+
+	<!-- end scripts -->
+
+	<!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+       mathiasbynens.be/notes/async-analytics-snippet -->
+	<script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+</body>
+</html>
+
---
+++ js/script.js
@@ -0,0 +1,158 @@
+var link1 = $('#slideshow-photo-1');
+var link2 = $('#slideshow-photo-2');
+var link3 = $('#slideshow-photo-3');
+var link4 = $('#slideshow-photo-4');
+var doalert = 0;
+var totalSlides = 5
+
+
+	
+var slideshow = new Dragdealer('slideshow',
+		  {
+		  	steps: totalSlides,
+		  	loose: true,
+		  	callback: function() {
+		  		
+		  		
+		  		
+		  	},
+		  	animationCallback: function(x, y)
+		  	{
+		  	
+		  		
+		  		var totalval = x*(totalSlides-1);
+		  		
+		  		if(totalval == (totalSlides-1)) {
+		  			
+		  			var doalert = 1;
+		  			
+		  		} else {
+		  			
+		  			var doalert = 0;
+		  		}
+		  		
+		  		if(doalert == 1){
+		  			 showHand();
+		  		setTimeout('finalPage()',2000);
+		  		setTimeout('hideHand()',5000);
+		  		
+		  		
+		  		}
+		  	}
+		  });
+
+
+function finalPage() 
+{
+	
+	var onevalueB = parseFloat(slideshow.getClosestSteps(slideshow.value.current));
+		var totalvalB = onevalueB*(totalSlides-1);
+	
+	if(totalvalB == (totalSlides-1)){
+		
+	showAlert();
+
+	 
+}
+}
+
+function showHand() {
+	 $('#handPhone').animate({
+		    opacity: 1,
+		    bottom: '0',
+		  
+		  }, 2000, function() {
+		    // Animation complete.
+			  
+		  });
+}
+
+function hideHand() {
+	$('#handPhone').animate({
+	    opacity: 0,
+	    bottom: '-200',
+	  
+	  }, 2000, function() {
+	    // Animation complete.
+		  
+	  });
+}
+
+function showAlert() {
+	 $('#finishedShopping').animate({
+		    opacity: 1
+		  
+		  
+		  }, 500, function() {
+		    // Animation complete.
+			  
+		  });
+}
+
+function hideAlert() {
+	$('#finishedShopping').animate({
+	    opacity: 0
+	 
+	  
+	  }, 1000, function() {
+	    // Animation complete.
+		  
+	  });
+}
+
+
+
+
+$(document).ready(function(){
+	
+	
+		 showHand();
+		 setTimeout('hideHand()',5000);
+
+		 document.getElementById('slideshow-photo-1').onclick = function()
+		    {
+		        slideshow.setStep(1);
+		        return false;
+		    }
+		    document.getElementById('slideshow-photo-2').onclick = function()
+		    {
+		        slideshow.setStep(2);
+		        return false;
+		    }
+		    document.getElementById('slideshow-photo-3').onclick = function()
+		    {
+		    
+		        slideshow.setStep(3);
+		        
+		        return false;
+		    }
+		    document.getElementById('slideshow-photo-4').onclick = function()
+		    {
+		        slideshow.setStep(4);
+		        return false;
+		    }
+		    document.getElementById('slideshow-photo-5').onclick = function()
+		    {
+		        slideshow.setStep(5);
+		        return false;
+		    }
+		    
+		    document.getElementById('slideshow-next').onclick = function()
+		    {
+		    	var valuenow = parseFloat(slideshow.getClosestSteps(slideshow.value.current));
+		    	var currentpage =(valuenow*(totalSlides-1)+1);
+		        slideshow.setStep(currentpage+1);
+		        return false;
+		    }
+		    
+		    document.getElementById('slideshow-previous').onclick = function()
+		    {
+		    	var valuenow = parseFloat(slideshow.getClosestSteps(slideshow.value.current));
+		    	var currentpage =(valuenow*(totalSlides-1)+1);
+		        slideshow.setStep(currentpage-1);
+		        return false;
+		    }
+  
+  
+  
+});

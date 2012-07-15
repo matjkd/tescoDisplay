f071166d93da772316a9378d14d1649026d36965
Mat Sadler <mat@redstudio.co.uk>
2012-07-15 02:59:55 +0100

    added pulsate

--- index.php
+++ index.php
@@ -2,129 +2,144 @@
 
 <html class="no-js" lang="en">
 
-<head>
-<meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
 
 
 
-<title></title>
-<meta name="description" content="">
+        <title></title>
+        <meta name="description" content="">
 
-<!-- Mobile viewport optimized: h5bp.com/viewport -->
-<meta name="viewport" content="width=device-width">
+        <!-- Mobile viewport optimized: h5bp.com/viewport -->
+        <meta name="viewport" content="width=device-width">
 
-<!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
 
-<link rel="stylesheet" href="css/dragdealer.css">
-
-
-
-
-</head>
-<body>
-
-	<div class="top-spaced">
-		<div id="slideshow" class="dragdealer">
-			<div class="handle" style="left: 0px;">
-				<div class="slide img1"></div>
-				<div class="slide img2"></div>
-				<div class="slide img3"></div>
-				<div class="slide img4"></div>
-				<div class="slide img5"></div>
-			</div>
-		</div>
-	</div>
-
-<!-- <div style="display: block;" id="slideshow-menu-wrapper"
-		class="top-spaced right-float">
-		<div id="slideshow-menu-cursor" class="cursor" style="top: 66px;"></div>
-		<ul id="slideshow-menu">
-			<li><a id="slideshow-photo-1" href="#photo1"> <span class="title"></span>
-					<span class="description">Page 1</span>
-			</a>
-			</li>
-			<li><a id="slideshow-photo-2" href="#photo2"> <span class="title"></span>
-					<span class="description">Page 2</span>
-			</a>
-			</li>
-			<li><a id="slideshow-photo-3" href="#photo3"> <span class="title"></span>
-					<span class="description">Page 3</span>
-			</a>
-			</li>
-			<li><a id="slideshow-photo-4" href="#photo4"> <span class="title"> </span>
-					<span class="description">Page 4</span>
-			</a>
-			</li>
-			<li><a id="slideshow-photo-5" href="#photo5"> <span class="title"> </span>
-					<span class="description">Page 5</span>
-			</a>
-			</li>
+        <link rel="stylesheet" href="css/dragdealer.css">
 
 
 
-		</ul>
-	</div> -->	
 
-	<div class="selectDisable" id="previousButton"
-		style="">
-		&nbsp;
-	</div>
+    </head>
+    <body>
 
-	<div class="selectDisable" id="nextButton"
-		style="">
-		&nbsp;
-	</div>
+        <div class="top-spaced">
+            <div id="slideshow" class="dragdealer">
+                <div class="handle" style="left: 0px;">
+                    <div class="slide img1"></div>
+                    <div class="slide img2"></div>
+                    <div class="slide img3"></div>
+                    <div class="slide img4"></div>
+                    <div class="slide img5"></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <div style="display: block;" id="slideshow-menu-wrapper"
+                        class="top-spaced right-float">
+                        <div id="slideshow-menu-cursor" class="cursor" style="top: 66px;"></div>
+                        <ul id="slideshow-menu">
+                                <li><a id="slideshow-photo-1" href="#photo1"> <span class="title"></span>
+                                                <span class="description">Page 1</span>
+                                </a>
+                                </li>
+                                <li><a id="slideshow-photo-2" href="#photo2"> <span class="title"></span>
+                                                <span class="description">Page 2</span>
+                                </a>
+                                </li>
+                                <li><a id="slideshow-photo-3" href="#photo3"> <span class="title"></span>
+                                                <span class="description">Page 3</span>
+                                </a>
+                                </li>
+                                <li><a id="slideshow-photo-4" href="#photo4"> <span class="title"> </span>
+                                                <span class="description">Page 4</span>
+                                </a>
+                                </li>
+                                <li><a id="slideshow-photo-5" href="#photo5"> <span class="title"> </span>
+                                                <span class="description">Page 5</span>
+                                </a>
+                                </li>
+        
+        
+        
+                        </ul>
+                </div> -->	
+
+        <div class="selectDisable" id="previousButton"
+             style="">
+            &nbsp;
+        </div>
+
+        <div class="selectDisable" id="nextButton"
+             style="">
+            &nbsp;
+        </div>
 
 
-	<div class="selectDisable" id="handPhone">
-	
-	<img id="bgImage" src="css/assets/handphone.png"/>
-	<div class="phoneSlideshow"style="display:none;">
-	
-	<img class="slideImages" src="css/assets/phonePage1.png"/>
-	<img class="slideImages" src="css/assets/phonePage2.png"/>
-	<img class="slideImages" src="css/assets/phonePage3.png"/>
-	<img class="slideImages" src="css/assets/phonePage4.png"/>
-	</div>
-	
-	<div class="phoneSlideshowSmall" style="display:none;">
-	
-	<img class="slideImagesSmall" src="css/assets/phonePage1.png"/>
-	<img class="slideImagesSmall" src="css/assets/phonePage2.png"/>
-	<img class="slideImagesSmall" src="css/assets/phonePage3.png"/>
-	<img class="slideImagesSmall" src="css/assets/phonePage4.png"/>
-	</div>
+        <div class="selectDisable" id="handPhone">
 
-	</div>
+            <img id="bgImage" src="css/assets/handphone.png"/>
+            <div class="phoneSlideshow"style="display:none;">
 
-	<div id="finishedShopping">
-		<img class="slideImages" src="css/assets/finished-shopping.png"/>
-		
-		
-		<div class="yesnocontainer">
-			<div onclick="reset()" class="selectDisable yesButton yesno">&nbsp;</div>
-			<div onclick="notFinished()" class="selectDisable noButton yesno">&nbsp;</div>
-		</div>
-	</div>
+                <img class="slideImages" src="css/assets/phonePage1.png"/>
+                <img class="slideImages" src="css/assets/phonePage2.png"/>
+                <img class="slideImages" src="css/assets/phonePage3.png"/>
+                <img class="slideImages" src="css/assets/phonePage4.png"/>
+            </div>
 
-	<!-- JavaScript at the bottom for fast page loading -->
+            <div class="phoneSlideshowSmall" style="display:none;">
 
-	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-	<script src="js/libs/jquery.js"></script>
+                <img class="slideImagesSmall" src="css/assets/phonePage1.png"/>
+                <img class="slideImagesSmall" src="css/assets/phonePage2.png"/>
+                <img class="slideImagesSmall" src="css/assets/phonePage3.png"/>
+                <img class="slideImagesSmall" src="css/assets/phonePage4.png"/>
+            </div>
 
-	<!-- scripts concatenated and minified via build script -->
-	<script src="js/dragdealer.js"></script>
-	<script src="js/cycle.js"></script>
-	<script src="js/idle.js"></script>
-	<script src="js/script.js"></script>
-	<script>
+        </div>
+
+        <div id="start">
+ <div class="startSlideshow" style="display:none;">
+
+                <img class="slidestart" src="css/assets/start-1.png"/>
+                <img class="slidestart" src="css/assets/start-2.png"/>
+                
+            </div>
+        </div>
+        
+        
+        <div id="tescoLogo" onclick="reset()">
+        </div>
+        <div id="finishedShopping">
+            <img  src="css/assets/finished-shopping.png"/>
+
+
+            <div class="yesnocontainer">
+                <div onclick="finished()" class="selectDisable yesButton yesno">&nbsp;</div>
+                <div onclick="notFinished()" class="selectDisable noButton yesno">&nbsp;</div>
+                
+            </div>
+            
+            <img id="goodflight" style="display:none" src="css/assets/have-a-good-flight.png"/>
+        </div>
+
+        <!-- JavaScript at the bottom for fast page loading -->
+
+        <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+        <script src="js/libs/jquery.js"></script>
+ 
+        <!-- scripts concatenated and minified via build script -->
+        <script src="js/dragdealer.js"></script>
+        <script src="js/cycle.js"></script>
+        <script src="js/idle.js"></script>
+        <script src="js/script.js"></script>
+        <script>
   
 
-  </script>
+        </script>
 
-	<!-- end scripts -->
+        <!-- end scripts -->
 
 
-</body>
+    </body>
 </html>
 
--- js/script.js
+++ js/script.js
@@ -8,147 +8,152 @@ var valuenow = 0;
 var currentpage = 0;
 var hand = 0;
 var phonesmall = 0;
-var timeout = 45000;
+var timeout = 20000;
 var d = new Date();
 var hour = d.getHours();
 var handclicked = 0;
 
 var slideshow = new Dragdealer('slideshow', {
-	steps : totalSlides,
-	loose : false,
-	speed : 20,
+    steps : totalSlides,
+    loose : false,
+    speed : 20,
 
-	loose : true,
-	callback : function() {
+    loose : true,
+    callback : function() {
 
-	},
-	animationCallback : function(x, y) {
+    },
+    animationCallback : function(x, y) {
 
-		var totalval = x * (totalSlides - 1);
+        var totalval = x * (totalSlides - 1);
 
-		if (totalval == 0) {
+        if (totalval == 0) {
 
-			var doalert = 1;
+            var doalert = 1;
 
-		} else if (totalval == 1) {
-			var doalert = 2;
+        } else if (totalval == 1) {
+            var doalert = 2;
 
-		} else if (totalval == 2) {
-			var doalert = 3;
-		} else if (totalval == 3) {
-			var doalert = 4;
-		} else if (totalval == 4) {
-			var doalert = 5;
-		}
+        } else if (totalval == 2) {
+            var doalert = 3;
+        } else if (totalval == 3) {
+            var doalert = 4;
+        } else if (totalval == 4) {
+            var doalert = 5;
+        }
 
-		else {
+        else {
 
-			var doalert = 0;
-		}
+            var doalert = 0;
+        }
 
-		if (doalert == 1) {
-			logAction('Page 1 touched');
-		}
+        if (doalert == 1) {
+            $('.startSlideshow').fadeIn();	
+            logAction('Page 1 touched');
+        }
 
-		if (doalert == 2) {
+        if (doalert == 2) {
+            
+            $('.startSlideshow').fadeOut('10');	
 
-			if (hand == 0) {
-				showHand();
+            if (hand == 0) {
+                showHand();
 
-				 $('.phoneSlideshow').cycle({
-						fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+                $('.phoneSlideshow').cycle({
+                    fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
 							
-					});
+                });
 				 
-				 $('.phoneSlideshowSmall').cycle({
-						fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+                $('.phoneSlideshowSmall').cycle({
+                    fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
 							
-					});
-				setTimeout('hideHand()', 17000);
-				hand = 1;
-			}
+                });
+                setTimeout('hideHand()', 17000);
+                hand = 1;
+            }
 
-			logAction('Page 2 Visited');
-		}
+            logAction('Page 2 Visited');
+        }
 
-		if (doalert == 3) {
-			logAction('Page 3  Visited');
-		}
+        if (doalert == 3) {
+             $('.startSlideshow').fadeOut();	
+            logAction('Page 3  Visited');
+        }
 
-		if (doalert == 4) {
-			logAction('Page 4  Visited');
-		}
+        if (doalert == 4) {
+             $('.startSlideshow').fadeOut();	
+            logAction('Page 4  Visited');
+        }
 
-		if (doalert == 5) {
+        if (doalert == 5) {
+		 $('.startSlideshow').fadeOut();		
+            setTimeout('finalPage()', 2000);
 			
-			setTimeout('finalPage()', 2000);
-			
-			logAction('Page 5 Visited');
-		}
+            logAction('Page 5 Visited');
+        }
 
-	}
+    }
 });
 function logAction(action) {
 
-	$.post("logger.php", {
-		action : action
-	});
+    $.post("logger.php", {
+        action : action
+    });
 }
 function notFinished() {
-	logAction('Clicked No on Finished Shopping popup');
-	hideAlert();
+    logAction('Clicked No on Finished Shopping popup');
+    hideAlert();
 }
 
 function finalPage() {
 
-	var onevalueB = parseFloat(slideshow
-			.getClosestSteps(slideshow.value.current));
-	var totalvalB = onevalueB * (totalSlides - 1);
+    var onevalueB = parseFloat(slideshow
+        .getClosestSteps(slideshow.value.current));
+    var totalvalB = onevalueB * (totalSlides - 1);
 
-	if (totalvalB == (totalSlides - 1)) {
+    if (totalvalB == (totalSlides - 1)) {
 
-		showAlert();
+        showAlert();
 
-	}
+    }
 }
 
 function showHand() {
-	$('.phoneSlideshowSmall').fadeOut();
-	$('#handPhone').animate({
-		opacity : 1,
-		bottom : '0',
-		left : '80',
+    $('.phoneSlideshowSmall').fadeOut();
+    $('#handPhone').animate({
+        opacity : 1,
+        bottom : '0',
+        left : '80',
 		
-		width: '768px'
-	}, 3000, function() {
-		// Animation complete.
-		if(handclicked == 0){
-		$('.phoneSlideshow').fadeIn();
-		} else {
-			//do nothing
-		}
-		phonesmall = 0;
-	});
+        width: '768px'
+    }, 3000, function() {
+        // Animation complete.
+        if(handclicked == 0){
+            $('.phoneSlideshow').fadeIn();
+        } else {
+        //do nothing
+        }
+        phonesmall = 0;
+    });
 	
 	
 }
 
 function hideHand() {
-	$('.phoneSlideshow').fadeOut('fast', function() {
-	    // Animation complete.
-		$('#handPhone').animate({
-			opacity : 1,
-			bottom : '-950',
-			left : '590px',
-			width: '492px'
+    $('.phoneSlideshow').fadeOut('fast', function() {
+        // Animation complete.
+        $('#handPhone').animate({
+            opacity : 1,
+            bottom : '-950',
+            left : '590px',
+            width: '492px'
 
-		}, 3000, function() {
-			// Animation complete.
-			$('.phoneSlideshowSmall').fadeIn();
-			phonesmall = 1;
+        }, 3000, function() {
+            // Animation complete.
+            $('.phoneSlideshowSmall').fadeIn();
+            phonesmall = 1;
 		
-		});
-	  });
+        });
+    });
 	
 	
 	
@@ -157,104 +162,126 @@ function hideHand() {
 
 }
 function hideHandtotal() {
-	$('#handPhone').animate({
-		opacity : 1,
-		bottom : '-1283',
-		left : '590px'
+    $('#handPhone').animate({
+        opacity : 1,
+        bottom : '-1283',
+        left : '590px'
 
-	}, 3000, function() {
-		// Animation complete.
+    }, 3000, function() {
+        // Animation complete.
 		
-	});
+        });
 	
 	
 }
+$(function(){
+    // Self-executing recursive animation
+    (function pulse(){
+        $('#nextButton').delay(200).animate({opacity: 0.3}, 2000 ).delay(50).animate({opacity:1}, 2000);
+        $('#previousButton').delay(200).animate({opacity: 0.3}, 2000 ).delay(50).animate({opacity:1}, 2000);
+        $('#start').delay(200).animate({opacity: 0.3}, 2000 ).delay(50).animate({opacity:1}, 2000,pulse);
+    })();
+});
 
 function showAlert() {
-	$('#finishedShopping').fadeIn('slow', function() {
-		// Animation complete
-	});
+    $('#finishedShopping').fadeIn('slow', function() {
+        // Animation complete
+        });
 }
 
 function hideAlert() {
-	$('#finishedShopping').fadeOut('slow', function() {
-		// Animation complete
-	});
+    $('#finishedShopping').fadeOut('slow', function() {
+        // Animation complete
+        });
 }
 
+function finished() {
+     $('#goodflight').fadeIn('slow', function() {
+        // Animation complete
+           setTimeout('reset()', 5000);
+        });
+}
 function reset() {
-	d = new Date();
-	hour = parseInt(d.getHours());
+    d = new Date();
+    hour = parseInt(d.getHours());
 	
-	if(hour > 21 || hour < 6) {
+    if(hour > 21 || hour < 6) {
 		
-		window.location  = 'slideshow.php';
-	} else {
+        window.location  = 'slideshow.php';
+    } else {
 	
 	
-	hideHandtotal();
-	location.reload();
-	}
+        hideHandtotal();
+        location.reload();
+    }
 }
 
 // Links
 
 $(document).ready(
-		function() {
+    function() {
+        
+        $('.startSlideshow').cycle({
+            fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+speed: 1000,
+timeout:5000
+        });
+	 $('.startSlideshow').fadeIn();		
+         
+
 			
-			
 
-	          $(document).bind("idle.idleTimer", function(){
-	        	  var timeoutCorrect = (timeout/1000);
-	        	  logAction('No Activity for ' + timeoutCorrect + ' seconds');
-	        	  reset();
-	          });
+        $(document).bind("idle.idleTimer", function(){
+            var timeoutCorrect = (timeout/1000);
+            logAction('No Activity for ' + timeoutCorrect + ' seconds');
+            reset();
+        });
 
-	          $(document).bind("active.idleTimer", function(){
-	             //user active again
-	          });
+        $(document).bind("active.idleTimer", function(){
+            //user active again
+            });
 
-	          $.idleTimer(timeout);
+        $.idleTimer(timeout);
 
 	         
 			
-			$('img').bind('dragstart', function(event) {
-				event.preventDefault();
-			});
-			// $(document).bind("contextmenu",function(e){
-			// return false;
-			// });
+        $('img').bind('dragstart', function(event) {
+            event.preventDefault();
+        });
+        // $(document).bind("contextmenu",function(e){
+        // return false;
+        // });
 			
-			$('#handPhone').click(function() {
+        $('#handPhone').click(function() {
 				
-				handclicked = 1;
+            handclicked = 1;
 				
-				if(phonesmall ==1){
-					hideHandtotal();
-				} else {
-					hideHand();
-				}
-			});
+            if(phonesmall ==1){
+                hideHandtotal();
+            } else {
+                hideHand();
+            }
+        });
 			
 
-			document.getElementById('nextButton').onclick = function() {
+        document.getElementById('nextButton').onclick = function() {
 
-				valuenow = parseFloat(slideshow
-						.getClosestSteps(slideshow.value.current));
-				currentpage = (valuenow * (totalSlides - 1) + 1);
-				// $('#nextButton').append(currentpage+1 + ' ');
-				slideshow.setStep(currentpage + 1);
-				return false;
-			}
+            valuenow = parseFloat(slideshow
+                .getClosestSteps(slideshow.value.current));
+            currentpage = (valuenow * (totalSlides - 1) + 1);
+            // $('#nextButton').append(currentpage+1 + ' ');
+            slideshow.setStep(currentpage + 1);
+            return false;
+        }
 
-			document.getElementById('previousButton').onclick = function() {
+        document.getElementById('previousButton').onclick = function() {
 
-				valuenow = parseFloat(slideshow
-						.getClosestSteps(slideshow.value.current));
-				currentpage = (valuenow * (totalSlides - 1) + 1);
-				// $('#previousButton').append(currentpage-1 + ' ');
-				slideshow.setStep(currentpage - 1);
-				return false;
-			}
+            valuenow = parseFloat(slideshow
+                .getClosestSteps(slideshow.value.current));
+            currentpage = (valuenow * (totalSlides - 1) + 1);
+            // $('#previousButton').append(currentpage-1 + ' ');
+            slideshow.setStep(currentpage - 1);
+            return false;
+        }
 
-		});
+    });
